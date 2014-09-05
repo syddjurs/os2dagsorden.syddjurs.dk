ab432cd0c7806874d4b9dbc7657033018c9e8624
Thomas Thune Hansen <tth@bellcom.dk>
2014-09-05 14:03:43 +0200

    first commit

---
+++ public_html/profiles/os2dagsorden/modules/contrib/autologout/autologout.js
@@ -0,0 +1,65 @@
+(function ($) {
+  Drupal.behaviors.autologout = {
+    attach: function(context, settings) {
+
+      var t = setTimeout(init, Drupal.settings.autologout.timeout);
+      var paddingTimer;
+
+      function init() {
+        dialog();
+        paddingTimer = setTimeout(logout, Drupal.settings.autologout.timeout_padding);
+      }
+
+      function dialog() {
+        $("<div> " +  Drupal.settings.autologout.message + "</div>").dialog({
+          modal: true,
+          closeOnEscape: false,
+          width: "auto",
+          buttons: {
+            "Fortsæt": function() {
+              $(this).dialog("destroy");
+              clearTimeout(paddingTimer);
+              refresh();
+            },
+            "Log af": function() {
+              $(this).dialog("destroy");
+              logout();
+            }
+          },
+          title: Drupal.settings.autologout.title,
+          close: function(event, ui) {
+            logout();
+          }
+        });
+      }
+
+      function logout() {
+        $.ajax({
+          url: Drupal.settings.basePath + "autologout_ahah_logout",
+          type: "POST",
+          success: function() {
+            window.location = Drupal.settings.autologout.redirect_url;
+          },
+          error: function(XMLHttpRequest, textStatus) {
+	window.location = Drupal.settings.autologout.redirect_url;
+           //alert("There has been an error resetting your last access time: " + textStatus + ".")
+          },
+        });
+      }
+
+      function refresh() {
+        $.ajax({
+          url: Drupal.settings.basePath + "autologout_ahah_set_last",
+          type: "POST",
+          success: function() {
+            t = setTimeout(init, Drupal.settings.autologout.timeout);
+          },
+          error: function(XMLHttpRequest, textStatus) {
+            alert("There has been an error resetting your last access time: " + textStatus + ".")
+          },
+        });
+      }
+
+    }
+  };
+})(jQuery);
